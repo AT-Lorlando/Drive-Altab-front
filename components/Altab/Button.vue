ac9f2efde7166a2fdade31f950b962b50d8395a2
Jérémy <rcd.jeremy@gmail.com>
2022-04-08 17:15:39 +0200

    Modal password v1

---
+++ components/Altab/Button.vue
@@ -0,0 +1,35 @@
+<template>
+    <button 
+        :class="`transition-colors duration-300 ease-in transform hover:scale-110 w-20 rounded-xl hover:cursor-pointer px-2 h-12 ${add_class}`">
+
+        {{Title}}
+    </button>
+</template>
+<script>
+export default {
+    props: {
+        Title: {
+            type: String,
+            default: 'Button'
+        },
+        Color: {
+            type: String,
+            default: 'black'
+        }
+    },
+    data() {
+        if (this.Color == 'black') {
+            return {
+                add_class: 'bg-black hover:bg-opacity-0 text-white hover:text-black border hover:border-black'
+            }
+        } else if (this.Color == 'transparent') {
+            return {
+                add_class: 'bg-transparent hover:bg-black hover:text-white border border-black hover:border-white'
+            }
+        }
+    },
+}
+</script>
+<style>
+    
+</style>
--- components/Home/Container.vue
+++ components/Home/Container.vue
@@ -1,14 +1,37 @@
 <template>
-    <div class="flex flex-row w-full h-full bg-black">
+    <div class="flex flex-col items-center w-full h-full bg-black">
     
         <canvas id="threecanvas" ref="canvas" class="absolute w-screen h-screen inset-0 z-0"></canvas>
         <!-- TODO: Sparkles -->
         <div class="grid grid-cols-5 w-full overflow-y-auto pt-12 px-12"> 
-            <div  v-for="f in folders" v-show="!folder_focus || f.clicked" :key="f.id" class="h-80 w-auto z-10 hover:cursor-pointer" @click="onClick(f)">
+            <div v-for="f in folders" v-show="!folder_focus || f.clicked" :key="f.id" class="h-80 w-auto z-10 hover:cursor-pointer" @click="onClick(f)">
                 <h2 class="break-normal text-white text-center uppercase text-xl absolute w-80">{{f.title}}</h2>
                 <div :id="`scene${f.id}`" @mouseover="hover(f)" class="w-80 h-80"/>
             </div>
         </div>
+
+        <div v-if="lockedModal" class="absolute mt-56 z-20 w-80 h-80 bg-white flex flex-col rounded-xl border-2 border-white py-2 bg-opacity-75 justify-between">
+            <div class="flex flex-row justify-between items-center border-b-2 border-black px-4 pb-1">
+                <h2 class="text-2xl text-center uppercase">{{folder_focus.title}}</h2>
+                <div class="flex flex-row items-center">
+                    <IconsExit class="w-10 h-10 hover:cursor-pointer" @click="lockedModal = false"/>
+                </div>
+            </div>
+            <div class="px-2">
+                <h2 class="text-black text-xl text-center pb-1">This folder is locked</h2>
+                <div class="flex flex-row justify-center items-center">
+                    <input type="password" v-model="password" class="w-full h-12 p-2 border-2 rounded-xl border-black text-black text-center" placeholder="Password"/>
+                </div>
+                <a class="pl-2 underline text-blue-600 hover:cursor-pointer hover:text-blue-800">How can i get the password</a>
+            </div>
+            <div class="w-full">
+                <div class="flex flex-row w-full justify-between px-2">                    
+                    <AltabButton @click="onClick(lockedModal)" Title="Cancel" Color="black"/>
+                    <AltabButton @click="onClick(lockedModal)" Title="Confirm" Color="transparent"/>
+                </div>
+            </div>
+        </div>
+
     </div>
 </template>
 
@@ -24,16 +47,16 @@ const scenes = []
 const mouse = new THREE.Vector2()
 
 class Folder {
-    constructor(id, title) {
+    constructor(id, title, password='') {
         this.id = id
         this.title = title
-        this.password = ''
+        this.password = password
         this.clicked = false
     }
 }
 
 const FOLDERS = [
-    new Folder(1, 'folder 1'),
+    new Folder(1, 'folder 1', "azerty"),
     new Folder(2, 'folder 2'),
     new Folder(3, 'folder 3'),
     new Folder(4, 'folder 4'),
@@ -46,6 +69,7 @@ export default {
         return {
             folders: FOLDERS,
             folder_focus: null,
+            lockedModal: false,
         }
     },
     asyncData({isDev, route, store, env, params, query, req, res, redirect, error}) {
@@ -102,11 +126,11 @@ export default {
                 camera.position.z = 2.5;
                 scene.userData.camera = camera;
 
-                const controls = new OrbitControls( scene.userData.camera, scene.userData.element );
-                controls.enablePan = false;
-                controls.enableZoom = false;
-                controls.enableRotate = false;
-                scene.userData.controls = controls;
+                // const controls = new OrbitControls( scene.userData.camera, scene.userData.element );
+                // controls.enablePan = false;
+                // controls.enableZoom = false;
+                // controls.enableRotate = false;
+                // scene.userData.controls = controls;
 
                 // add 8 plane forming a cube
                 const cube = new THREE.Group()
@@ -166,11 +190,26 @@ export default {
                 const rect = renderer.domElement.getBoundingClientRect()
                 const width = rect.right - rect.left;
                 const height = rect.bottom - rect.top;
+                
                 renderer.setViewport( 0, 0, width, height );
                 renderer.setScissor( 0, 0, width, height );
+                renderer.render( scene, cameraGlobal );
+
+                // const element = scene.userData.element;
+                // const rect = element.getBoundingClientRect();
+                // // set the viewport
+                // const width = rect.right - rect.left;
+                // const height = rect.bottom - rect.top;
+                // const left = rect.left;
+                // const bottom = renderer.domElement.clientHeight - rect.bottom;
+                // renderer.setViewport( left, bottom, width, height );
+                // renderer.setScissor( left, bottom, width, height );   
+                // const camera = scene.userData.camera;
+                // renderer.render( scene, camera );
+
+
                 // camera.position.y = Math.cos( t + index ) *0.8  ;
 
-                renderer.render( scene, cameraGlobal );
             } else {
                 scenes.forEach((s, index) => {
                     const element = s.userData.element;
@@ -199,6 +238,16 @@ export default {
             console.log(f)
             f.clicked = !f.clicked
             this.folder_focus = this.folder_focus? null : f
+
+            // Change the class of the clicked element
+            const element = document.getElementById( `scene${f.id}` );
+            console.log(element.parentElement.classList)
+            element.parentElement.classList.toggle('hover:cursor-pointer')
+
+            // Show lockedModal if the folder is locked
+            if(f.password) {
+                this.lockedModal = true
+            }
         },
         
     },
