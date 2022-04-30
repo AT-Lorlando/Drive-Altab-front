1f5c00d467271b2c4491ff419edd8609d6ca19b4
Jérémy <rcd.jeremy@gmail.com>
2022-04-30 14:38:05 +0200

    Icons

---
+++ components/Icons/LeftArrow.vue
@@ -0,0 +1,5 @@
+<template>
+    <svg class="stroke-current" width="64" height="64" viewBox="0 0 64 64" fill="none">
+        <path d="M24 32L23.6464 31.6464L23.2929 32L23.6464 32.3536L24 32ZM39.6464 15.6464L23.6464 31.6464L24.3536 32.3536L40.3536 16.3536L39.6464 15.6464ZM23.6464 32.3536L39.6464 48.3536L40.3536 47.6464L24.3536 31.6464L23.6464 32.3536Z" stroke="current" stroke-linecap="round"/>
+    </svg>
+</template>
---
+++ components/Icons/RightArrow.vue
@@ -0,0 +1,5 @@
+<template>
+    <svg class="stroke-current" width="64" height="64" viewBox="0 0 64 64" fill="none">
+        <path d="M40 32L40.3536 31.6464L40.7071 32L40.3536 32.3536L40 32ZM24.3536 15.6464L40.3536 31.6464L39.6464 32.3536L23.6464 16.3536L24.3536 15.6464ZM40.3536 32.3536L24.3536 48.3536L23.6464 47.6464L39.6464 31.6464L40.3536 32.3536Z" stroke="current" stroke-linecap="round"/>
+    </svg>
+</template>
